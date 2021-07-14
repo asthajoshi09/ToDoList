44fe4e51b42c5ef6577bc7b654d944cb2b6deea5
Astha Joshi <78919783+asthajoshi09@users.noreply.github.com>
2021-07-14 11:33:19 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodocomponentComponent } from './todocomponent/todocomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodocomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todocomponent/todocomponent.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todocomponent',
+  templateUrl: './todocomponent.component.html',
+  styleUrls: ['./todocomponent.component.scss']
+})
+export class TodocomponentComponent implements OnInit {
+  todolist: any[] = [];
+  error: string = "";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addTask(item: string) {
+
+    if (item) {
+      this.todolist.push({ id: this.todolist.length, name: item, isCompleted: false });
+      this.error = "";
+    }
+    else {
+      this.error = "Task can't be empty";
+    }
+  }
+
+  strike(todoObj: any) {
+    this.todolist.map((toDo, index) => {
+      if (toDo.id == todoObj.id) {
+        this.todolist[index]['isCompleted'] = !todoObj['isCompleted']
+      }
+    })
+
+  }
+  taskCompleted() {
+    let count = 0;
+    this.todolist.map((toDo, index) => {
+      if (toDo.isCompleted) {
+        count++
+      }
+    })
+    return count;
+  }
+
+
+}
